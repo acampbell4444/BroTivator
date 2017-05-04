d67bf561d4a2877cc08efb02c1d1395a7f77de0d
Alan Campbell <alancampbell4444@gmail.com>
2017-05-04 11:59:07 -0700

    linted

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -1,12 +1,6 @@
 import React, {Component} from 'react'
 
 class Team extends Component {
-  
-  constructor(props) {
-    super(props)
-    //this.state = {}
-  }
-
   render(){
   const {user} = this.props
   console.log(user)
