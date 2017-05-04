ff40e0f300e46806c96d1315ac31166a51526225
Alan Campbell <alancampbell4444@gmail.com>
2017-05-04 14:43:45 -0700

    linted

--- app/components/NewTeamModal.jsx
+++ app/components/NewTeamModal.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, ButtonToolbar, Modal} from 'react-bootstrap'
+import { Button, ButtonToolbar, Modal } from 'react-bootstrap'
 import { browserHistory } from 'react-router'
 
 export default class extends Component {
